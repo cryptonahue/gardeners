67cc26e850acb18f50ada35c9578ffd07b372b76
CryptoNahue <87866711+cryptonahue@users.noreply.github.com>
2022-09-16 15:51:36 -0300

    cambios

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,15 @@
+import isLoggedIn from '../../login/login';
+
+import Card from '@mui/material/Card';
+
+function About() {
+
+
+
+  return <div>About</div>
+
+isLoggedIn()
+
+}
+
+export default About
